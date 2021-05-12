7d0c6ba57dce56a6a252a48b8b8d1df6249168cb
VladyslavHolovko <vladyslav.holovko@storeworkflows.com>
2021-05-12 18:59:16 +0300

    fix: 'findDOMNode is deprecated in StrictMode' console error issue

--- client/src/components/MovieCard/index.js
+++ client/src/components/MovieCard/index.js
@@ -5,43 +5,49 @@ import Grow from '@material-ui/core/Grow';
 import IconButton from "@material-ui/core/IconButton";
 import CardContent from '@material-ui/core/CardContent';
 import CancelIcon from '@material-ui/icons/Cancel';
+import { unstable_createMuiStrictModeTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+//To avoid 'findDOMNode is deprecated in StrictMode' console error
+const theme = unstable_createMuiStrictModeTheme();
 
 const MovieCard = ({ movie, showDeleteButton, onDelete }) => {
     return (
-        <Grow in={true} timeout={400}>
-            <Card className={`movie-card ${showDeleteButton && '_show-delete'}`}>
-                <CardContent className="movie-card__content">
-                    <div className="movie-card__header">
-                    <span>
-                        {movie.releaseYear}
-                    </span>
+        <ThemeProvider theme={theme}>
+            <Grow in={true} timeout={400}>
+                <Card className={`movie-card ${showDeleteButton && '_show-delete'}`}>
+                    <CardContent className="movie-card__content">
+                        <div className="movie-card__header">
                         <span>
-                        {movie.format}
-                    </span>
-                    </div>
-                    <h3 className="movie-card__title">
-                        {movie.title}
-                    </h3>
-                    {showDeleteButton &&
-                        <IconButton
-                            className="movie-card__delete-button"
-                            onClick={onDelete}
-                        >
-                            <CancelIcon className="movie-card__button-icon" fontSize="small"/>
-                        </IconButton>
-                    }
-                    <div className="movie-card__footer">
-                        {movie.stars.map((star, i) => (
-                            <p className="movie-card__star" key={i}>
-                                {star}
-                                {(i !== movie.stars.length - 1) && ','}
-                            </p>
-                        ))}
-                    </div>
-                </CardContent>
-            </Card>
-        </Grow>
-
+                            {movie.releaseYear}
+                        </span>
+                            <span>
+                            {movie.format}
+                        </span>
+                        </div>
+                        <h3 className="movie-card__title">
+                            {movie.title}
+                        </h3>
+                        {showDeleteButton &&
+                            <IconButton
+                                className="movie-card__delete-button"
+                                onClick={onDelete}
+                            >
+                                <CancelIcon className="movie-card__button-icon" fontSize="small"/>
+                            </IconButton>
+                        }
+                        <div className="movie-card__footer">
+                            {movie.stars.map((star, i) => (
+                                <p className="movie-card__star" key={i}>
+                                    {star}
+                                    {(i !== movie.stars.length - 1) && ','}
+                                </p>
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
+            </Grow>
+        </ThemeProvider>
     );
 };
 
